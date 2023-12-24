ba727fbcd48888feef71b0e62b6b75582323e973
Paul Sisson <paulsissonsemail@gmail.com>
2023-12-24 10:43:18 -0600

    Add more collections methods

--- python_collections.py
+++ python_collections.py
@@ -49,14 +49,14 @@ websters = {
   "b": "b words",
   "c": "c words",
 }
-websters_defaults = defaultdict(lambda: '<- that + words')
+websters_defaults = defaultdict(lambda: '<- + words')
 websters_defaults.update(websters)
 # print(websters_defaults)
 # defaultdict(<function <lambda> at 0x1025ca3e0>, {'a': 'a words', 'b': 'b words', 'c': 'c words'})
 for letter in letters_of_the_alphabet:
   websters[letter] = websters_defaults[letter]
 # print(websters)
-# {'a': 'a words', 'b': 'b words', 'c': 'c words', 'd': '<- that + words', 'e': '<- that + words', 'f': '<- that + words', 'g': '<- that + words', 'h': '<- that + words', 'i': '<- that + words', 'j': '<- that + words', 'k': '<- that + words', 'l': '<- that + words', 'm': '<- that + words', 'n': '<- that + words', 'o': '<- that + words', 'p': '<- that + words', 'q': '<- that + words', 'r': '<- that + words', 's': '<- that + words', 't': '<- that + words', 'u': '<- that + words', 'v': '<- that + words', 'w': '<- that + words', 'x': '<- that + words', 'y': '<- that + words', 'z': '<- that + words'}
+# {'a': 'a words', 'b': 'b words', 'c': 'c words', 'd': '<- + words', 'e': '<- + words', 'f': '<- + words', 'g': '<- + words', 'h': '<- + words', 'i': '<- + words', 'j': '<- + words', 'k': '<- + words', 'l': '<- + words', 'm': '<- + words', 'n': '<- + words', 'o': '<- + words', 'p': '<- + words', 'q': '<- + words', 'r': '<- + words', 's': '<- + words', 't': '<- + words', 'u': '<- + words', 'v': '<- + words', 'w': '<- + words', 'x': '<- + words', 'y': '<- + words', 'z': '<- + words'}
 
 # --------------------------------
 # Next: OrderedDict()
@@ -105,6 +105,106 @@ for item in to_remove:
 # Order: 7 returned
 # Order: 9 returned
 # Order: 12 returned
+
+
+profits = [
+    {'jan_profit': 15492.30, 'jan_holiday_profit': 2589.12},
+    {'feb_profit': 17018.05, 'feb_holiday_profit': 3701.88},
+    {'mar_profit': 11849.13},
+    {'apr_profit': 9870.68},
+    {'may_profit': 13662.34},
+    {'jun_profit': 12903.54},
+    {'jul_profit': 16965.08, 'jul_holiday_profit': 4360.21},
+    {'aug_profit': 17685.69},
+    {'sep_profit': 9815.57},
+    {'oct_profit': 10318.28},
+    {'nov_profit': 23295.43, 'nov_holiday_profit': 9896.55},
+    {'dec_profit': 21920.19, 'dec_holiday_profit': 8060.79}
+]
+
+profits_the_sequel = [
+    {'jan_profit': 13977.85, 'jan_holiday_profit': 2176.43},
+    {'feb_profit': 16692.15, 'feb_holiday_profit': 3239.74},
+    {'mar_profit': 17524.35, 'mar_holiday_profit': 4301.92}
+]
+
+# --------------------------
+# Next: ChainMap()
+# -- This lets you chain dictionaries together to create one giant dictionary
+# -- You can also add more dictionaries to the original giant one using the new_child() method
+# --------------------------
+profit_map = ChainMap(*profits)
+
+def get_profits(profit_data):
+  total_standard_profits = 0.0
+  total_holiday_profits = 0.0
+  for key in profit_data.keys():
+    if 'holiday' not in key:
+      total_standard_profits += profit_data[key]
+    else:
+      total_holiday_profits += profit_data[key]
+  return total_standard_profits, total_holiday_profits
+
+last_year_standard_profit, last_year_holiday_profit = get_profits(profit_map)
+# print(last_year_standard_profit, last_year_holiday_profit)
+# 180796.27999999997 28608.55
+for item in profits_the_sequel:
+  profit_map = profit_map.new_child(item)
+
+current_year_standard_profit, current_year_holiday_profit = get_profits(profit_map)
+# print(current_year_standard_profit, current_year_holiday_profit)
+# 184631.15 32035.64
+year_standard_so_far = current_year_standard_profit - last_year_standard_profit
+year_holiday_so_far = current_year_holiday_profit - last_year_holiday_profit
+# print(year_standard_so_far, year_holiday_so_far)
+# 3834.8700000000244 3427.09
+
+# --------------------------
+# Next: Counter()
+# -- This does basically what you'd assume it does with a little extra functionality that's cool.
+# -- This might actually be my favorite of the collections module so far
+# --------------------------
+a_list_of_numbers = [1,2,3,2,1,5,6,2,3,6,7,5,7,9,6,5,8,0,5,6,8,7,9,4,5,1,3,6,8,2,6,9,8,4,5,6,6,2,3,6,5,5,9,5]
+another_list_of_numbers = [2,5,4,8,7,9,6,5,4,8,2,1,5,3,4,7,5,1,2,3,6,5,4,7,8,2,2,4,8,6,2,1,7,5,6,2,3,1,4,8,6]
+counted_numbers = Counter(a_list_of_numbers)
+more_counted_numbers = Counter(another_list_of_numbers)
+# print(counted_numbers)
+# Counter({5: 9, 6: 9, 2: 5, 3: 4, 9: 4, 8: 4, 1: 3, 7: 3, 4: 2, 0: 1})
+# print(more_counted_numbers)
+# Counter({2: 7, 5: 6, 4: 6, 8: 5, 6: 5, 7: 4, 1: 4, 3: 3, 9: 1})
+
+# some cool methods for this class
+added_numbers = counted_numbers + more_counted_numbers
+# print(added_numbers)
+# Counter({5: 15, 6: 14, 2: 12, 8: 9, 4: 8, 1: 7, 3: 7, 7: 7, 9: 5, 0: 1})
+
+minus_numbers = counted_numbers - more_counted_numbers
+# print(minus_numbers)
+# Counter({6: 4, 5: 3, 9: 3, 3: 1, 0: 1}) <-- this removes negative values and zeroed out values
+
+# print(counted_numbers)
+# Counter({5: 9, 6: 9, 2: 5, 3: 4, 9: 4, 8: 4, 1: 3, 7: 3, 4: 2, 0: 1})
+counted_numbers.subtract(more_counted_numbers)
+# print(counted_numbers)
+# Counter({6: 4, 5: 3, 9: 3, 3: 1, 0: 1, 1: -1, 7: -1, 8: -1, 2: -2, 4: -4})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
